c418a949cccc2b9013568e29c6b579465efb71c7
Lotnest <lotnestyt@gmail.com>
2024-12-21 17:51:33 +0100

    fix: Fix vulnerability allowing message interception in guild chat fix: Fix fabric-api version for compatibility with Minecraft 1.21

--- common/src/main/java/dev/lotnest/sequoia/ws/SequoiaWebSocketClient.java
+++ common/src/main/java/dev/lotnest/sequoia/ws/SequoiaWebSocketClient.java
@@ -16,6 +16,14 @@ import dev.lotnest.sequoia.ws.messages.SMessageWSMessage;
 import dev.lotnest.sequoia.ws.messages.session.GIdentifyWSMessage;
 import dev.lotnest.sequoia.ws.messages.session.SSessionResultWSMessage;
 import dev.lotnest.sequoia.wynn.guild.GuildService;
+import java.net.URI;
+import java.time.OffsetDateTime;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.TimeUnit;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import net.minecraft.network.chat.ClickEvent;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.HoverEvent;
@@ -25,15 +33,6 @@ import net.neoforged.bus.api.SubscribeEvent;
 import org.apache.commons.lang3.StringUtils;
 import org.java_websocket.client.WebSocketClient;
 import org.java_websocket.handshake.ServerHandshake;
-
-import java.net.URI;
-import java.time.OffsetDateTime;
-import java.util.Map;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ConcurrentLinkedQueue;
-import java.util.concurrent.TimeUnit;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public final class SequoiaWebSocketClient extends WebSocketClient {
     private static final String WS_DEV_URL = "ws://localhost:8085/sequoia-tree/ws";
@@ -207,20 +206,22 @@ public final class SequoiaWebSocketClient extends WebSocketClient {
                     }
                 }
                 case SSessionResult -> {
-                    SSessionResultWSMessage sSessionResultWSMessage = GSON.fromJson(message, SSessionResultWSMessage.class);
+                    SSessionResultWSMessage sSessionResultWSMessage =
+                            GSON.fromJson(message, SSessionResultWSMessage.class);
                     SSessionResultWSMessage.Data sSessionResultWSMessageData =
                             sSessionResultWSMessage.getSessionResultData();
 
                     if (sSessionResultWSMessageData.error()) {
                         new Thread(() -> {
-                            try {
-                                Thread.sleep(120000);
-                                authenticate(false);
-                            } catch (Exception exception) {
-                                SequoiaMod.debug("Failed to reconnect to WebSocket server: "
-                                        + exception.getMessage() + " - " + sSessionResultWSMessageData.result());
-                            }
-                        })
+                                    try {
+                                        Thread.sleep(120000);
+                                        authenticate(false);
+                                    } catch (Exception exception) {
+                                        SequoiaMod.debug(
+                                                "Failed to reconnect to WebSocket server: " + exception.getMessage()
+                                                        + " - " + sSessionResultWSMessageData.result());
+                                    }
+                                })
                                 .start();
                         return;
                     }
@@ -249,7 +250,8 @@ public final class SequoiaWebSocketClient extends WebSocketClient {
                                     Component.literal(url).withStyle(style -> style.withColor(0x1DA1F2)
                                             .withClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, url))
                                             .withHoverEvent(new HoverEvent(
-                                                    HoverEvent.Action.SHOW_TEXT, Component.literal("Click to open URL")))));
+                                                    HoverEvent.Action.SHOW_TEXT,
+                                                    Component.literal("Click to open URL")))));
 
                             lastMatchEnd = matcher.end();
                         }
