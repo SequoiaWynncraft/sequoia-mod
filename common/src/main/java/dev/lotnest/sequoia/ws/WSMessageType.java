80c443c1c01280216850bde73cace78862906751
Lotnest <lotnestyt@gmail.com>
2025-01-15 18:42:54 +0100

    fix: Make player's guild deduction independent on Wynncraft's API fix: Party Finder message filter pattern refactor: Update the code based on SonarQube gate quality report chore: Bump Wynntils version

--- common/src/main/java/dev/lotnest/sequoia/ws/WSMessageType.java
+++ common/src/main/java/dev/lotnest/sequoia/ws/WSMessageType.java
@@ -1,41 +1,41 @@
 package dev.lotnest.sequoia.ws;
 
 public enum WSMessageType {
-    Invalid(0),
+    INVALID(0),
 
-    GChatMessage(1),
-    GClientCommand(2),
-    GRaidSubmission(3),
-    GLocationService(4),
-    GGuildMap(5),
-    GIdentify(6),
-    GGuildWarResults(7),
-    GLootPool(8),
-    GReserved5(9),
-    GReserved6(10),
-    GReserved7(11),
-    GReserved8(12),
-    GReserved9(13),
-    GReserved10(14),
+    G_CHAT_MESSAGE(1),
+    G_CLIENT_COMMAND(2),
+    G_RAID_SUBMISSION(3),
+    G_LOCATION_SERVICE(4),
+    G_GUILD_MAP(5),
+    G_IDENTIFY(6),
+    G_GUILD_WAR_RESULTS(7),
+    G_LOOT_POOL(8),
+    G_RESERVED_5(9),
+    G_RESERVED_6(10),
+    G_RESERVED_7(11),
+    G_RESERVED_8(12),
+    G_RESERVED_9(13),
+    G_RESERVED_10(14),
 
-    SChannelMessage(15),
-    SCommandData(16),
-    SCommandResult(17),
-    SChatMessageBroadcast(18),
-    SCommandPipe(19),
-    SRaidSubmission(20),
-    SMessage(21),
-    SSessionResult(22),
-    SReserved5(23),
-    SReserved6(24),
-    SReserved7(25),
-    SReserved8(26),
-    SReserved9(27),
-    SReserved10(28),
+    S_CHANNEL_MESSAGE(15),
+    S_COMMAND_DATA(16),
+    S_COMMAND_RESULT(17),
+    S_CHAT_MESSAGE_BROADCAST(18),
+    S_COMMAND_PIPE(19),
+    S_RAID_SUBMISSION(20),
+    S_MESSAGE(21),
+    S_SESSION_RESULT(22),
+    S_RESERVED_5(23),
+    S_RESERVED_6(24),
+    S_RESERVED_7(25),
+    S_RESERVED_8(26),
+    S_RESERVED_9(27),
+    S_RESERVED_10(28),
 
-    DChannelMessage(29),
-    DGetConnectedClient(30),
-    DServerRestart(31);
+    D_CHANNEL_MESSAGE(29),
+    D_GET_CONNECTED_CLIENT(30),
+    D_SERVER_RESTART(31);
 
     private final int value;
 
@@ -49,7 +49,7 @@ public enum WSMessageType {
                 return type;
             }
         }
-        return Invalid;
+        return INVALID;
     }
 
     public int getValue() {
