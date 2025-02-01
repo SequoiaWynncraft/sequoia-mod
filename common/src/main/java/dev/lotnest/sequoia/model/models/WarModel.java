f594cf5dedc27e8e466c4b0016f63a6fffceaaa1
Lotnest <lotnestyt@gmail.com>
2025-02-01 14:22:03 +0100

    chore: Add WarModel base

---
+++ common/src/main/java/dev/lotnest/sequoia/model/models/WarModel.java
@@ -0,0 +1,12 @@
+package dev.lotnest.sequoia.model.models;
+
+import dev.lotnest.sequoia.model.Model;
+
+import java.util.List;
+
+public class WarModel extends Model {
+
+    public WarModel() {
+        super(List.of());
+    }
+}
