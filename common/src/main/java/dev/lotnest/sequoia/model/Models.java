4832ae7836e152002fea827f05e7e48b9b7c279f
Lotnest <lotnestyt@gmail.com>
2025-02-01 14:21:11 +0100

    chore: Add models

---
+++ common/src/main/java/dev/lotnest/sequoia/model/Models.java
@@ -0,0 +1,10 @@
+package dev.lotnest.sequoia.model;
+
+import dev.lotnest.sequoia.model.models.WarModel;
+
+public final class Models {
+    public static final WarModel War = new WarModel();
+
+    private Models() {
+    }
+}
