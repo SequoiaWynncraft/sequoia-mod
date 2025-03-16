e855eb10dcdd2a33fc569fe587d9ee57bbca2bda
Lotnest <lotnestyt@gmail.com>
2025-03-16 22:53:35 +0100

    feat: Enhance SChannelMessageWSMessageHandler to include guild rank in messages

--- common/src/main/java/dev/lotnest/sequoia/core/ws/handler/ws/SChannelMessageWSMessageHandler.java
+++ common/src/main/java/dev/lotnest/sequoia/core/ws/handler/ws/SChannelMessageWSMessageHandler.java
@@ -6,12 +6,15 @@ package dev.lotnest.sequoia.core.ws.handler.ws;
 
 import static dev.lotnest.sequoia.core.ws.WSConstants.GSON;
 
+import com.wynntils.models.guild.type.GuildRank;
 import com.wynntils.utils.mc.McUtils;
 import dev.lotnest.sequoia.SequoiaMod;
 import dev.lotnest.sequoia.core.text.Fonts;
 import dev.lotnest.sequoia.core.ws.handler.WSMessageHandler;
 import dev.lotnest.sequoia.core.ws.message.ws.discordchatbridge.SChannelMessageWSMessage;
+import java.util.Locale;
 import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
 
 public class SChannelMessageWSMessageHandler extends WSMessageHandler {
     public SChannelMessageWSMessageHandler(String message) {
@@ -25,13 +28,29 @@ public class SChannelMessageWSMessageHandler extends WSMessageHandler {
             SChannelMessageWSMessage sChannelMessageWSMessage = (SChannelMessageWSMessage) wsMessage;
             SChannelMessageWSMessage.Data sChannelMessageWSMessageData =
                     sChannelMessageWSMessage.getSChannelMessageData();
+            String[] displayNameSplit =
+                    sChannelMessageWSMessageData.displayName().split(" ");
+            String playerName = displayNameSplit.length > 1 ? displayNameSplit[1] : displayNameSplit[0];
 
             McUtils.sendMessageToClient(Fonts.BannerPill.parse("discord")
-                    .withStyle(ChatFormatting.AQUA)
-                    .append(Fonts.Default.parse(" " + sChannelMessageWSMessageData.displayName() + ": ")
-                            .withStyle(ChatFormatting.DARK_AQUA))
+                    .withColor(0x7289DA)
+                    .append(Component.literal(" "))
+                    .append(Fonts.BannerPill.parse(getGuildRank(sChannelMessageWSMessageData.sequoiaRoles()))
+                            .withStyle(ChatFormatting.AQUA))
+                    .append(Fonts.Default.parse(" " + playerName + ": ").withStyle(ChatFormatting.DARK_AQUA))
                     .append(Fonts.Default.parse(sChannelMessageWSMessageData.message())
                             .withStyle(ChatFormatting.AQUA)));
         }
+    }
+
+    private String getGuildRank(String[] discordRoles) {
+        for (GuildRank guildRank : GuildRank.values()) {
+            for (String discordRole : discordRoles) {
+                if (guildRank.getName().equalsIgnoreCase(discordRole)) {
+                    return guildRank.getName().toLowerCase(Locale.ROOT);
+                }
+            }
+        }
+        return GuildRank.RECRUIT.getName().toLowerCase(Locale.ROOT);
     }
 }
